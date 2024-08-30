3d100fbb6a8745f19d8c1b55025f035ad965b03e
Athul Rajagopalan P <athulrajagopalan88@gmail.com>
2024-08-30 10:47:38 +0530

    vercel.json updated

--- bizkit-card/src/components/BizcardDetailView.jsx
+++ bizkit-card/src/components/BizcardDetailView.jsx
@@ -43,33 +43,33 @@ const BizCardDetailView = () => {
   }
 
   return (
-    <div className="flex justify-center w-full items-center min-h-screen bg-[#08353B]">
-    <div className="bg-[#111314] rounded-lg shadow-lg p-6 w-80">
-      {bizCard.logo && (
-        <div className="flex justify-center mb-4">
-          <div className="rounded-full w-28 h-28 overflow-hidden border border-gray-700">
-            <img
-              src={`data:image/jpeg;base64,${bizCard.logo}`}
-              alt="Company Logo"
-              className="h-full w-full object-cover"
-            />
+    <div className="flex justify-center w-full items-center min-h-screen bg-[#08353B] p-4">
+      <div className="bg-[#111314] rounded-lg shadow-lg p-6 w-full max-w-md">
+        {bizCard.logo && (
+          <div className="flex justify-center mb-4">
+            <div className="rounded-full w-24 h-24 sm:w-28 sm:h-28 overflow-hidden border border-gray-700">
+              <img
+                src={`data:image/jpeg;base64,${bizCard.logo}`}
+                alt="Company Logo"
+                className="h-full w-full object-cover"
+              />
+            </div>
           </div>
+        )}
+        <h1 className="text-xl sm:text-2xl font-bold mb-2 text-center text-[#09AD8A]">
+          {bizCard.name || 'N/A'}
+        </h1>
+        <div className="text-center mb-6">
+          <p className="text-white">{bizCard.designation || 'N/A'}</p>
+          <p className="text-white">{bizCard.company_name || 'N/A'}</p>
         </div>
-      )}
-      <h1 className="text-2xl font-bold mb-2 text-center text-[#09AD8A]">
-        {bizCard.name || 'N/A'}
-      </h1>
-      <div className="text-center mb-6">
-        <p className="text-white">{bizCard.designation || 'N/A'}</p>
-        <p className="text-white">{bizCard.company_name || 'N/A'}</p>
-      </div>
-      <div className="text-center">
-        <button className="bg-[#09AD8A] text-white py-2 px-4 rounded hover:bg-[#09AD8A]-600">
-          Download Now ⬇️  
-        </button>
+        <div className="text-center">
+          <button className="bg-[#09AD8A] text-white py-2 px-4 rounded hover:bg-[#09AD8A]-600">
+            Download Now ⬇️  
+          </button>
+        </div>
       </div>
     </div>
-  </div>
 );
 };
 
